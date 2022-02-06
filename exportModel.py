c213dae3e22f9bd16f9b444fcddf0038f43c1c59
Dominik <70096457+scout507@users.noreply.github.com>
2022-02-06 15:19:23 +0100

    Addtional comments

--- exportModel.py
+++ exportModel.py
@@ -1,6 +1,8 @@
 import tensorflowjs as tfjs
 from tensorflow import keras
 
+# THIS IS USED FOR EXPORTING EXISTING MODELS  
+
 # Load the model
 model = keras.models.load_model("testmodel1_1mio")
 # Export the model
